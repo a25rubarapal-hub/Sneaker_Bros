b852f02b3252fa1ab2951733b8f0b630190ee998
Qg-sp <quim.garciasolano@gmail.com>
2026-04-21 17:09:23 +0200

    pozolon

---
+++ Assets/Scripts/PozoTeleport.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class PozoTeleport : MonoBehaviour
+{
+    public Transform destino;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        Movimiento player = collision.GetComponent<Movimiento>();
+
+        if (player != null && player.IsTeleporting == false && Input.GetKey(KeyCode.S))
+        {
+            StartCoroutine(Teleport(player));
+        }
+    }
+
+    private IEnumerator Teleport(Movimiento player)
+    {
+        player.SetTeleporting(true);
+        player.CanMove = false;
+
+        yield return new WaitForSeconds(0.2f);
+
+        player.transform.position = destino.position;
+
+        yield return new WaitForSeconds(0.2f);
+
+        player.CanMove = true;
+        player.SetTeleporting(false);
+    }
+}
