3afee2047250c824f1d5cc9a4ffc9a02c27713d9
Qg-sp <quim.garciasolano@gmail.com>
2026-04-21 17:18:04 +0200

    Update PozoTeleport.cs

--- Assets/Scripts/PozoTeleport.cs
+++ Assets/Scripts/PozoTeleport.cs
@@ -26,7 +26,7 @@ public class PozoTeleport : MonoBehaviour
 
         player.transform.position = destino.position;
 
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(1.0f);
 
         player.CanMove = true;
         player.SetTeleporting(false);
