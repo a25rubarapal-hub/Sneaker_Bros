83b1160f4263a63c4b521941ed773c2686e72517
Platinum <a25rubarapal@inspedralbes.cat>
2026-05-12 22:40:46 +0200

    fafafafafafaf

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    void Start()
+    {
+        Vector2 pos = GuardadoJava.Cargar();
+        transform.position = pos;
+        Debug.Log("📥 Cargando");
+    }
+}
---
+++ Assets/Scripts/SavePoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SavePoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Vector3 pos = col.transform.position;
+            GuardadoJava.Guardar(pos.x, pos.y);
+            Debug.Log("💾 Partida Guardada");
+        }
+    }
+}
